6ccc17f25ce99d16281707e5680df4f6b20bd792
Rick Lamers <ricklamers@gmail.com>
2020-07-21 12:31:43 +0200

    add require lib build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,22 +1,22 @@
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
 import { NotebookTools, INotebookTracker } from '@jupyterlab/notebook';
+import { IDocumentManager } from '@jupyterlab/docmanager';
 /**
  * The plugin registration information.
  */
 declare const plugin: JupyterFrontEndPlugin<void>;
 declare global {
     interface Window {
-        app: JupyterFrontEnd;
-        tracker: INotebookTracker;
+        _orchest_app: JupyterFrontEnd;
+        _orchest_tracker: INotebookTracker;
+        _orchest_docmanager: IDocumentManager;
     }
 }
 /**
  * A notebook extension that adds visual cell tags to Notebook cells.
  */
 export declare class OrchestIntegrationExtension extends NotebookTools.Tool {
-    private tracker;
-    private app;
-    constructor(tracker: INotebookTracker, app: JupyterFrontEnd);
+    constructor(tracker: INotebookTracker, app: JupyterFrontEnd, docmanager: IDocumentManager);
 }
 /**
  * Export the plugin as default.
